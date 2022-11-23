3203693e9817ccd529b3933d8e75b9e2ec28b426
Mkorewo <korewo.marcel@gmail.com>
2022-11-23 15:44:14 +0100

    Add project files.

---
+++ configFile/DataRequestMenager.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace configFile
+{
+    internal class DataRequestMenager
+    {
+        public string GetExchangeRates()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string result="";
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://api.nbp.pl/api/exchangerates/tables/A/last/1?format=json");
+                var responseMessage = httpClient.SendAsync(request).Result;
+                var responseContent = responseMessage.Content.ReadAsStringAsync().Result;
+
+                RateInfoArray[] rateInfoArray = JsonConvert.DeserializeObject<RateInfoArray[]>(responseContent);
+                foreach(var item in rateInfoArray.First().rates)
+                {
+                    result += $"{item.currency}: {item.mid}\n";
+                }
+
+                return result;
+            }
+        }
+    }
+    class RateInfoArray
+    {
+        public string table { get; set; }
+        public string no { get; set; }
+        public string effectiveDate { get; set; }
+        public Rates[] rates { get; set; }
+    }
+    class Rates
+    {
+        public string currency { get; set; }
+        public string code { get; set; }
+        public float mid { get; set; }
+    }
+}
---
+++ configFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace configFile
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataRequestMenager dataRequestMenager = new DataRequestMenager();
+            string path = Path.GetDirectoryName(typeof(Program).Assembly.Location)+"\\currencyConfigFile.txt";
+
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(dataRequestMenager.GetExchangeRates());   
+            }
+
+        }
+    }
+}
